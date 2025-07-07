cfeb264bcfd9134159024cad90a0fa959a3360cb
isthereanynameleft <nootnoot@mailbox.org>
2025-07-08 00:55:23 +0200

    update (#57)
    * add debug output (#51)
    
    * fix edit event button (#53)
    
    * 55 add layout update config (#56)

--- http_handler.py
+++ http_handler.py
@@ -5,6 +5,41 @@ import data_management as dm
 import logging_manager as lm
 import re
 
+
+class LayoutConfig(object):
+
+    current_event: bool
+    next_event: bool
+    locations: int
+    msg_otd: bool
+
+    def __init__(self, current_event= True, next_event=True, locations=1, msg_otd=True):
+        if locations <= 0:
+            raise ValueError("location Count must be positive!")
+        self.current_event = current_event
+        self.next_event = next_event
+        self.locations = locations
+        self.msg_otd = msg_otd
+
+def layout_conf_reader(line: str) -> LayoutConfig:
+    if not (line.startswith("<!-- LayoutConfig: ") and line.endswith(" -->")):
+        return LayoutConfig()
+    line = line[19:-4]
+    parameter = line.split(";")
+    conf = LayoutConfig()
+
+    # check bool parameter
+    conf.current_event = "current_event" in parameter
+    conf.next_event = "next_event" in parameter
+    conf.msg_otd = "msg_otd" in parameter
+
+    # check other parameter
+    for P in parameter:
+        if P.startswith("locations="):
+            conf.locations = int(P[10:])
+
+    return conf
+
 CLIENTS = {}
 preview_list = []
 layout_map = {}
@@ -12,13 +47,17 @@ location_map = {}
 layouts = {
     "default": ''
 }
+layout_conf = {
+    "default": LayoutConfig()
+}
 def __load_layouts():
-    global layouts, layout_map, location_map
+    global layouts, layout_map, location_map, layout_conf
     files = [F[:-5] for F in os.listdir("layouts") if F.endswith(".html")]
     files.sort()
     for F in files:
         html = open("layouts/"+F+".html").read()
         layouts[F] = html
+        layout_conf[F] = layout_conf_reader(html.split("\n")[0])
     try:
         location_map = json.load(open("./data/locations.json"))
         layout_map = json.load(open("./data/layouts.json"))
@@ -54,34 +93,47 @@ def get_table(request):
 
 async def send_data(ws):
     preview = ws in preview_list
-    loc = location_map.get(CLIENTS.get(ws))
     loc_prefix = re.compile("^\\[.*]")
-    lm.log("Sending Data to screen at", loc, preview)
+    layout = layout_map.get(CLIENTS.get(ws), "default")
+    conf = layout_conf.get(layout)
+    for li in range(conf.locations):
+        loc = location_map.get(CLIENTS.get(ws))[li]
+        lm.log("Sending Data to screen at", loc, preview)
+        suffix = ""
+        if li > 0:
+            suffix = "_" + str(li)
 
-    event, desc, start_str, duration = dm.get_current_event(loc, prefab=preview)
-    my_data = {"id": "event_name", "html": event}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "event_desc", "html": desc}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "event_start", "html": start_str[-5:].replace("-", ":")}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "event_len", "html": str(duration)}
-    await ws.send_str(json.dumps(my_data))
-
-    event, desc, start_str, duration = dm.get_next_event(loc, prefab=preview)
-    my_data = {"id": "event_next_name", "html": event}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "event_next_desc", "html": desc}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "event_next_start", "html": start_str[-5:].replace("-", ":")}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "event_next_len", "html": str(duration)}
-    await ws.send_str(json.dumps(my_data))
-
-    my_data = {"id": "msg_of_the_day", "html": dm.msg_of_the_day}
-    await ws.send_str(json.dumps(my_data))
-    my_data = {"id": "location", "html": loc_prefix.sub("", loc)}
-    await ws.send_str(json.dumps(my_data))
+        if conf.current_event:
+            event, desc, start_str, duration = dm.get_current_event(loc, prefab=preview)
+            if duration is not None:
+                my_data = {"id": "event_name"+suffix, "html": event}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_desc"+suffix, "html": desc}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_start"+suffix, "html": start_str[-5:].replace("-", ":")}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_end"+suffix, "html": dm.shift_timestamp(start_str, duration)[-5:].replace("-", ":")}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_len"+suffix, "html": str(duration)}
+                await ws.send_str(json.dumps(my_data))
+        if conf.next_event:
+            event, desc, start_str, duration = dm.get_next_event(loc, prefab=preview)
+            if duration is not None:
+                my_data = {"id": "event_next_name"+suffix, "html": event}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_next_desc"+suffix, "html": desc}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_next_start"+suffix, "html": start_str[-5:].replace("-", ":")}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_next_end"+suffix, "html": dm.shift_timestamp(start_str, int(duration))[-5:].replace("-", ":")}
+                await ws.send_str(json.dumps(my_data))
+                my_data = {"id": "event_next_len"+suffix, "html": str(duration)}
+                await ws.send_str(json.dumps(my_data))
+        my_data = {"id": "location"+suffix, "html": loc_prefix.sub("", loc)}
+        await ws.send_str(json.dumps(my_data))
+    if conf.msg_otd:
+        my_data = {"id": "msg_of_the_day", "html": dm.msg_of_the_day}
+        await ws.send_str(json.dumps(my_data))
 
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
@@ -100,7 +152,7 @@ async def websocket_handler(request):
                     preview_list.append(ws)
                 CLIENTS[ws] = screen_id
                 if location_map.get(screen_id) is None:
-                    location_map[screen_id] = "default"
+                    location_map[screen_id] = ["default"]
                 if layout_map.get(screen_id) is None:
                     layout_map[screen_id] = "default"
                 lm.log("adding screen:", msg.data, msg_type=lm.LogType.ScreenLogin)
